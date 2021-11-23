e035b041afbddda3e5743a426d3b8ff141801f8d
arkjiy <88699010+arkjiy@users.noreply.github.com>
2021-11-23 18:03:14 +0900

    숫자 입력 가능하도록 수정

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.buttonExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonExit
@@ -48,6 +52,70 @@
             this.buttonExit.UseVisualStyleBackColor = false;
             this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.DimGray;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label1.Location = new System.Drawing.Point(10, 253);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.MaximumSize = new System.Drawing.Size(380, 60);
+            this.label1.MinimumSize = new System.Drawing.Size(380, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(380, 60);
+            this.label1.TabIndex = 10;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("맑은 고딕", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.SkyBlue;
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label2.Location = new System.Drawing.Point(20, 173);
+            this.label2.Margin = new System.Windows.Forms.Padding(0);
+            this.label2.MaximumSize = new System.Drawing.Size(360, 50);
+            this.label2.MinimumSize = new System.Drawing.Size(360, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(360, 50);
+            this.label2.TabIndex = 11;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("맑은 고딕", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.SkyBlue;
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label3.Location = new System.Drawing.Point(20, 113);
+            this.label3.Margin = new System.Windows.Forms.Padding(0);
+            this.label3.MaximumSize = new System.Drawing.Size(360, 50);
+            this.label3.MinimumSize = new System.Drawing.Size(360, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(360, 50);
+            this.label3.TabIndex = 12;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("맑은 고딕", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.SkyBlue;
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label4.Location = new System.Drawing.Point(20, 53);
+            this.label4.Margin = new System.Windows.Forms.Padding(0);
+            this.label4.MaximumSize = new System.Drawing.Size(360, 50);
+            this.label4.MinimumSize = new System.Drawing.Size(360, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(360, 50);
+            this.label4.TabIndex = 13;
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
@@ -55,14 +123,21 @@
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(400, 750);
             this.ControlBox = false;
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonExit);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
             this.Name = "Form1";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
         private void CreatingNewButtons()
@@ -74,23 +149,24 @@
             for (int i = 0; i < buttonArray.Length; i++)
             {
                 buttonArray[i] = new Button();
+                buttonArray[i].Location = new Point(horizotal, vertical);
+                this.Controls.Add(buttonArray[i]);
+
                 if (i == 18)
                 {
                     buttonArray[i].Size = new Size(200, 82);
                 }
                 else
                     buttonArray[i].Size = new Size(100, 82);
-                buttonArray[i].Location = new Point(horizotal, vertical);
+
                 if ((i == 3) || (i == 7) || (i == 11) || (i == 15))
                 {
                     horizotal = 0;
-                    vertical = vertical + 82;
+                    vertical += 82;
                 }
                 else
-                    horizotal = horizotal + 100;
-                this.Controls.Add(buttonArray[i]);
-            }
-
+                    horizotal += 100;
+            }            
             //
             // buttonDesign
             //
@@ -99,209 +175,142 @@
                 buttonArray[i].FlatAppearance.BorderSize = 0;
                 buttonArray[i].FlatAppearance.BorderColor = Color.Gainsboro;
                 buttonArray[i].FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-                if((i == 3)||(i == 7)||(i == 11)||(i == 15))
+                buttonArray[i].Font = new System.Drawing.Font("맑은 고딕", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+                buttonArray[i].TabStop = false;
+                if ((i == 3)||(i == 7)||(i == 11)||(i == 15))
                 {
                     buttonArray[i].BackColor = System.Drawing.Color.PowderBlue;
+                    buttonArray[i].Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
                 }
                 else if(i == 18)
+                {
                     buttonArray[i].BackColor = System.Drawing.Color.SkyBlue;
+                    buttonArray[i].Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+                }                    
                 else
                     buttonArray[i].BackColor = System.Drawing.Color.WhiteSmoke;
             }
             //
             // buttonCE
             //
-            buttonArray[0].Font = new System.Drawing.Font("맑은 고딕", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            buttonArray[0].Margin = new System.Windows.Forms.Padding(0);
             buttonArray[0].Name = "buttonCE";
-            buttonArray[0].TabIndex = 9;
             buttonArray[0].Text = "CE";
-            buttonArray[0].UseVisualStyleBackColor = false;
             buttonArray[0].Click += new System.EventHandler(buttonCE_Click);
             //
             // buttonC
             //
-            buttonArray[1].Font = new System.Drawing.Font("맑은 고딕", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            buttonArray[1].Margin = new System.Windows.Forms.Padding(0);
             buttonArray[1].Name = "buttonC";
-            buttonArray[1].TabIndex = 9;
             buttonArray[1].Text = "C";
-            buttonArray[1].UseVisualStyleBackColor = false;
             buttonArray[1].Click += new System.EventHandler(buttonC_Click);
             //
             // buttonPercent
             //
-            buttonArray[2].Font = new System.Drawing.Font("맑은 고딕", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            buttonArray[2].Margin = new System.Windows.Forms.Padding(0);
             buttonArray[2].Name = "buttonPercent";
-            buttonArray[2].TabIndex = 9;
             buttonArray[2].Text = "%";
-            buttonArray[2].UseVisualStyleBackColor = false;
             buttonArray[2].Click += new System.EventHandler(buttonPercent_Click);
             //
             // buttonPlus
             //
-            buttonArray[3].Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            buttonArray[3].Margin = new System.Windows.Forms.Padding(0);
             buttonArray[3].Name = "buttonPlus";
-            buttonArray[3].TabIndex = 9;
             buttonArray[3].Text = "＋";
-            buttonArray[3].UseVisualStyleBackColor = false;
             buttonArray[3].Click += new System.EventHandler(buttonPlus_Click);
             //
             // button7
             //
-            buttonArray[4].Font = new System.Drawing.Font("맑은 고딕", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            buttonArray[4].Margin = new System.Windows.Forms.Padding(0);
             buttonArray[4].Name = "button7";
-            buttonArray[4].TabIndex = 9;
             buttonArray[4].Text = "7";
-            buttonArray[4].UseVisualStyleBackColor = false;
             buttonArray[4].Click += new System.EventHandler(button7_Click);
             //
             // button8
             //
-            buttonArray[5].Font = new System.Drawing.Font("맑은 고딕", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            buttonArray[5].Margin = new System.Windows.Forms.Padding(0);
             buttonArray[5].Name = "button8";
-            buttonArray[5].TabIndex = 9;
             buttonArray[5].Text = "8";
-            buttonArray[5].UseVisualStyleBackColor = false;
             buttonArray[5].Click += new System.EventHandler(button8_Click);
             //
             // button9
             //
-            buttonArray[6].Font = new System.Drawing.Font("맑은 고딕", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            buttonArray[6].Margin = new System.Windows.Forms.Padding(0);
             buttonArray[6].Name = "button9";
-            buttonArray[6].TabIndex = 9;
             buttonArray[6].Text = "9";
-            buttonArray[6].UseVisualStyleBackColor = false;
             buttonArray[6].Click += new System.EventHandler(button9_Click);
             //
             // buttonMinus
             //
-            buttonArray[7].Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            buttonArray[7].Margin = new System.Windows.Forms.Padding(0);
             buttonArray[7].Name = "buttonMinus";
-            buttonArray[7].TabIndex = 9;
             buttonArray[7].Text = "-";
-            buttonArray[7].UseVisualStyleBackColor = false;
             buttonArray[7].Click += new System.EventHandler(buttonMinus_Click);
             //
             // button4
             //
-            buttonArray[8].Font = new System.Drawing.Font("맑은 고딕", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            buttonArray[8].Margin = new System.Windows.Forms.Padding(0);
             buttonArray[8].Name = "button4";
-            buttonArray[8].TabIndex = 9;
             buttonArray[8].Text = "4";
-            buttonArray[8].UseVisualStyleBackColor = false;
             buttonArray[8].Click += new System.EventHandler(button4_Click);
             //
             // button5
             //
-            buttonArray[9].Font = new System.Drawing.Font("맑은 고딕", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            buttonArray[9].Margin = new System.Windows.Forms.Padding(0);
             buttonArray[9].Name = "button5";
-            buttonArray[9].TabIndex = 9;
             buttonArray[9].Text = "5";
-            buttonArray[9].UseVisualStyleBackColor = false;
             buttonArray[9].Click += new System.EventHandler(button5_Click);
             //
             // button6
             //
-            buttonArray[10].Font = new System.Drawing.Font("맑은 고딕", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            buttonArray[10].Margin = new System.Windows.Forms.Padding(0);
             buttonArray[10].Name = "button6";
-            buttonArray[10].TabIndex = 9;
             buttonArray[10].Text = "6";
-            buttonArray[10].UseVisualStyleBackColor = false;
             buttonArray[10].Click += new System.EventHandler(button6_Click);
             //
             // buttonMulti
             //
-            buttonArray[11].Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            buttonArray[11].Margin = new System.Windows.Forms.Padding(0);
             buttonArray[11].Name = "buttonMulti";
-            buttonArray[11].TabIndex = 9;
             buttonArray[11].Text = "×";
-            buttonArray[11].UseVisualStyleBackColor = false;
             buttonArray[11].Click += new System.EventHandler(buttonMulti_Click);
             //
             // button1
             //
-            buttonArray[12].Font = new System.Drawing.Font("맑은 고딕", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            buttonArray[12].Margin = new System.Windows.Forms.Padding(0);
             buttonArray[12].Name = "button1";
-            buttonArray[12].TabIndex = 9;
             buttonArray[12].Text = "1";
-            buttonArray[12].UseVisualStyleBackColor = false;
             buttonArray[12].Click += new System.EventHandler(button1_Click);
             //
             // button2
             //
-            buttonArray[13].Font = new System.Drawing.Font("맑은 고딕", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            buttonArray[13].Margin = new System.Windows.Forms.Padding(0);
             buttonArray[13].Name = "button2";
-            buttonArray[13].TabIndex = 9;
             buttonArray[13].Text = "2";
-            buttonArray[13].UseVisualStyleBackColor = false;
             buttonArray[13].Click += new System.EventHandler(button2_Click);
             //
             // button3
             //
-            buttonArray[14].Font = new System.Drawing.Font("맑은 고딕", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            buttonArray[14].Margin = new System.Windows.Forms.Padding(0);
             buttonArray[14].Name = "button3";
-            buttonArray[14].TabIndex = 9;
             buttonArray[14].Text = "3";
-            buttonArray[14].UseVisualStyleBackColor = false;
             buttonArray[14].Click += new System.EventHandler(button3_Click);
             //
             // buttonDiv
             //
-            buttonArray[15].Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            buttonArray[15].Margin = new System.Windows.Forms.Padding(0);
             buttonArray[15].Name = "buttonDiv";
-            buttonArray[15].TabIndex = 9;
             buttonArray[15].Text = "÷";
-            buttonArray[15].UseVisualStyleBackColor = false;
             buttonArray[15].Click += new System.EventHandler(buttonDiv_Click);
             //
             // button0
             //
-            buttonArray[16].Font = new System.Drawing.Font("맑은 고딕", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            buttonArray[16].Margin = new System.Windows.Forms.Padding(0);
             buttonArray[16].Name = "button0";
-            buttonArray[16].TabIndex = 9;
             buttonArray[16].Text = "0";
-            buttonArray[16].UseVisualStyleBackColor = false;
             buttonArray[16].Click += new System.EventHandler(button0_Click);
             //
             // buttonDot
             //
-            buttonArray[17].Font = new System.Drawing.Font("맑은 고딕", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            buttonArray[17].Margin = new System.Windows.Forms.Padding(0);
             buttonArray[17].Name = "buttonDot";
-            buttonArray[17].TabIndex = 9;
             buttonArray[17].Text = ".";
-            buttonArray[17].UseVisualStyleBackColor = false;
             buttonArray[17].Click += new System.EventHandler(buttonDot_Click);
             //
             // buttonResult
             //
-            buttonArray[18].Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            buttonArray[18].Margin = new System.Windows.Forms.Padding(0);
             buttonArray[18].Name = "buttonResult";
-            buttonArray[18].TabIndex = 9;
             buttonArray[18].Text = "＝";
-            buttonArray[18].UseVisualStyleBackColor = false;
             buttonArray[18].Click += new System.EventHandler(buttonResult_Click);
         }
 
         #endregion
         private Button buttonExit;
-
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -16,9 +16,9 @@ namespace AWESOME_Calculator
             int nWidthEllipse, // width of ellipse
             int nHeightEllipse // height of ellipse
         );
-
         public Form1()
         {
+            this.KeyPreview = true;
             InitializeComponent();
             Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
         }
@@ -43,49 +43,114 @@ namespace AWESOME_Calculator
         {
         }
         private void button7_Click(object sender, EventArgs e)
-        {0
+        {
+            print("7");
         }
         private void button8_Click(object sender, EventArgs e)
         {
+            print("8");
         }
         private void button9_Click(object sender, EventArgs e)
         {
+            print("9");
         }
         private void buttonMinus_Click(object sender, EventArgs e)
         {
         }
         private void button4_Click(object sender, EventArgs e)
         {
+            print("4");
         }
         private void button5_Click(object sender, EventArgs e)
         {
+            print("5");
         }
         private void button6_Click(object sender, EventArgs e)
         {
+            print("6");
         }
         private void buttonMulti_Click(object sender, EventArgs e)
         {
         }
         private void button1_Click(object sender, EventArgs e)
         {
+            print("1");
         }
         private void button2_Click(object sender, EventArgs e)
         {
+            print("2");
         }
         private void button3_Click(object sender, EventArgs e)
         {
+            print("3");
         }
         private void buttonDiv_Click(object sender, EventArgs e)
         {
         }
         private void button0_Click(object sender, EventArgs e)
         {
+            print("0");
         }
         private void buttonDot_Click(object sender, EventArgs e)
         {
         }
         private void buttonResult_Click(object sender, EventArgs e)
         {
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.NumPad0 :
+                case Keys.D0:
+                    print("0");
+                    break;
+                case Keys.NumPad1 :
+                case Keys.D1:
+                    print("1");
+                    break;
+                case Keys.NumPad2 :
+                case Keys.D2:
+                    print("2");
+                    break;
+                case Keys.NumPad3 :
+                case Keys.D3:
+                    print("3");
+                    break;
+                case Keys.NumPad4 :
+                case Keys.D4:
+                    print("4");
+                    break;
+                case Keys.NumPad5:
+                case Keys.D5 :
+                    print("5");
+                    break;
+                case Keys.NumPad6 :
+                case Keys.D6:
+                    print("6");
+                    break;
+                case Keys.NumPad7 :
+                case Keys.D7:
+                    print("7");
+                    break;
+                case Keys.NumPad8 :
+                case Keys.D8:
+                    print("8");
+                    break;
+                case Keys.NumPad9 :
+                case Keys.D9:
+                    print("9");
+                    break;                 
+            }
+        }
+        
+        private void print(string str)
+        {
+            if(str != "+" || str != "-" || str != "¡¿" || str != "¡À")
+            {
+                label1.Text += str;
+            }            
         }
     }
 }
